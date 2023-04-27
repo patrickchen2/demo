3652ede04133bc311729cb025eb453b68603fb4c
patrickchen2 <chenpatrick1303@gmail.com>
2023-04-27 11:20:10 +0100

    hi

--- Game.py
+++ Game.py
@@ -1,8 +1,14 @@
+from itertools import product
+from enum import auto
+
+class GameError(Exception):
+    pass
 
 class Game:
 
     P1 = 'o'
     P2 = 'x'
+    DRAW = auto()
     _EMPTY = ' '
     _DIM = 3
 
@@ -20,14 +26,27 @@ class Game:
         result += f"\n\n{self._player} turn to play"
         return result
 
-    def play(self,row,col):
+    def _validate(self,row,col):
+        if not (0 < row <= Game._DIM):
+            raise GameError(f"Row {row} not in range")
+        if not (0 < col <= Game._DIM):
+            raise GameError(f"Column {col} not in range")
         row -= 1
         col -= 1
+        return row,col
+
+    def play(self,row,col):
+        row,col = self._validate(row,col)
+        if self._board[row][col] is not Game._EMPTY:
+            raise GameError("You must play in an empty space")
         self._board[row][col] = self._player
         self._player = Game.P2 if self._player is Game.P1 else Game.P1
     
     @property
     def winner(self):
+        if all(self._board[r][c] is not Game._EMPTY for (r,c) in product(range(Game._DIM),range(Game._DIM))):
+            return Game.DRAW
+            
         for p in [Game.P1,Game.P2]:
             for row in range(Game._DIM):
                 if all(self._board[row][col] is p for col in range(Game._DIM)):
@@ -40,6 +59,10 @@ class Game:
             if all(self._board[i][Game._DIM - 1 - i] is p for i in range(Game._DIM)):
                 return p
         return None
+
+    @property
+    def player(self):
+        return self._player
 
 if __name__ == "__main__":
     g = Game()
